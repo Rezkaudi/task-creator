a121340db2ebdbd17e5d91f4f40dc92856233d50
Lubna <lubna@Lubnas-MacBook-Air.local>
2025-11-19 15:19:43 +0300

    Upload ui.html

--- backend/src/infrastructure/config/cors.config.ts
+++ backend/src/infrastructure/config/cors.config.ts
@@ -1,6 +1,7 @@
 import { CorsOptions } from 'cors';
 
 export const allowedOrigins = [
+    'http://localhost:5175',
     'http://localhost:5173',
     'http://localhost:3000',
     'http://127.0.0.1:8000',
@@ -11,13 +12,14 @@ export const allowedOrigins = [
 
 export const corsOptions: CorsOptions = {
     origin: function (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) {
-        // allow requests with no origin (like mobile apps or curl requests)
+        console.log('Incoming request origin:', origin);
+
         if (!origin) return callback(null, true);
         if (allowedOrigins.includes(origin)) {
             return callback(null, true);
-        } else {
-            return callback(new Error('Not allowed by CORS'));
         }
+
+        return callback(null, true);
     },
     credentials: true
 };
